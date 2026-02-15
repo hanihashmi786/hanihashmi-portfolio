0b536392e47bdbff08c512e0c0a72f9177e279e6
Hani Mustafa Hashmi <hanimhashmi121@gmail.com>
2026-02-15 14:00:00 +0300

    feat: setup Vue Router with navigation routes

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: { title: 'Home' }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+    meta: { title: 'About' }
+  },
+  {
+    path: '/experience',
+    name: 'experience',
+    component: () => import('../views/ExperienceView.vue'),
+    meta: { title: 'Experience' }
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: () => import('../views/PortfolioView.vue'),
+    meta: { title: 'Portfolio' }
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/ContactView.vue'),
+    meta: { title: 'Contact' }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title + ' - Hani Hashmi';
+  next();
+});
+
+export default router
